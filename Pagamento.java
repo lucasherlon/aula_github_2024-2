9845d9fcf1535cf1732d61e9ee79c79a99167cad
lucasherlon <lucasherlondsmc@gmail.com>
2024-10-06 11:31:17 -0300

    resolvendo as issues 6 e 7

---
+++ Pagamento.java
@@ -0,0 +1,30 @@
+public class Pagamento {
+    private double valor;
+    private String data;
+    private String hora;
+    private String descricao;
+
+    public Pagamento(double valor, String data, String hora, String descricao) {
+        this.valor = valor;
+        this.data = data;
+        this.hora = hora;
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+    
+}
